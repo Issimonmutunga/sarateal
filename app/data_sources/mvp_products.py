fb8fb91d07d6cd0c7d344b502cf79d7ac54ac791
Issimonmutunga <besimonmutunga@gmail.com>
2026-05-20 23:17:07 +0300

    Add MVP product seed data

---
+++ app/data_sources/mvp_products.py
@@ -0,0 +1,27 @@
+MVP_PRODUCTS = [
+    {
+        "name": "Maize",
+        "category": "cereal",
+        "unit": "kg",
+    },
+    {
+        "name": "Beans",
+        "category": "legume",
+        "unit": "kg",
+    },
+    {
+        "name": "Potatoes",
+        "category": "tuber",
+        "unit": "kg",
+    },
+    {
+        "name": "Tomatoes",
+        "category": "vegetable",
+        "unit": "kg",
+    },
+    {
+        "name": "Onions",
+        "category": "vegetable",
+        "unit": "kg",
+    },
+]
