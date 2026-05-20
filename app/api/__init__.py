61dda97b7cfc57c846ac14c1035ce51f72fc2804
Issimonmutunga <besimonmutunga@gmail.com>
2026-05-20 22:56:54 +0300

    Expose API routers

---
+++ app/api/__init__.py
@@ -0,0 +1,19 @@
+from app.api.buyers import router as buyers_router
+from app.api.buyer_demand import router as buyer_demand_router
+from app.api.counties import router as counties_router
+from app.api.farmers import router as farmers_router
+from app.api.farmer_supply import router as farmer_supply_router
+from app.api.matches import router as matches_router
+from app.api.products import router as products_router
+from app.api.tenders import router as tenders_router
+
+__all__ = [
+    "buyers_router",
+    "buyer_demand_router",
+    "counties_router",
+    "farmers_router",
+    "farmer_supply_router",
+    "matches_router",
+    "products_router",
+    "tenders_router",
+]
