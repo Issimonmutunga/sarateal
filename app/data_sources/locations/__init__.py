858f65873c17ef12fb2b4f5375b58a8d88d54bfc
Issimonmutunga <besimonmutunga@gmail.com>
2026-05-25 16:54:53 +0300

    Expose Kenya market coordinates

---
+++ app/data_sources/locations/__init__.py
@@ -0,0 +1,13 @@
+from app.data_sources.locations.kenya_market_coordinates import (
+    KENYA_MARKET_COORDINATES,
+    LocationCoordinate,
+    find_market_coordinate,
+    list_market_coordinates,
+)
+
+__all__ = [
+    "KENYA_MARKET_COORDINATES",
+    "LocationCoordinate",
+    "find_market_coordinate",
+    "list_market_coordinates",
+]
