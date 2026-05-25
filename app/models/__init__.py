0207c29ffac1fc87e60ab9715c427fabbaec6f0e
Issimonmutunga <besimonmutunga@gmail.com>
2026-05-25 14:53:35 +0300

    Register market model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,7 @@ from app.models.buyer_demand import BuyerDemand
 from app.models.county import County
 from app.models.farmer import Farmer
 from app.models.farmer_supply import FarmerSupply
+from app.models.market import Market
 from app.models.match import Match
 from app.models.product import Product
 from app.models.tender import Tender
@@ -13,6 +14,7 @@ __all__ = [
     "County",
     "Farmer",
     "FarmerSupply",
+    "Market",
     "Match",
     "Product",
     "Tender",
