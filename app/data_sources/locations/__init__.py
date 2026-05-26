b8cb82e77dede0f2bf1a8f0d6a4e2c64b61edabb
Issimonmutunga <besimonmutunga@gmail.com>
2026-05-26 09:59:33 +0300

    Expose Kenya county coordinates

--- app/data_sources/locations/__init__.py
+++ app/data_sources/locations/__init__.py
@@ -1,3 +1,9 @@
+from app.data_sources.locations.kenya_county_coordinates import (
+    KENYA_COUNTY_COORDINATES,
+    CountyCoordinate,
+    find_county_coordinate,
+    list_county_coordinates,
+)
 from app.data_sources.locations.kenya_market_coordinates import (
     KENYA_MARKET_COORDINATES,
     LocationCoordinate,
@@ -6,8 +12,12 @@ from app.data_sources.locations.kenya_market_coordinates import (
 )
 
 __all__ = [
+    "CountyCoordinate",
+    "KENYA_COUNTY_COORDINATES",
     "KENYA_MARKET_COORDINATES",
     "LocationCoordinate",
+    "find_county_coordinate",
     "find_market_coordinate",
+    "list_county_coordinates",
     "list_market_coordinates",
 ]
