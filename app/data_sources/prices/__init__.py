2c3ec1c879099a3285e5a7f644198658129d622f
Issimonmutunga <besimonmutunga@gmail.com>
2026-05-29 22:16:38 +0300

    Export WFP Kenya price adapter

--- app/data_sources/prices/__init__.py
+++ app/data_sources/prices/__init__.py
@@ -16,12 +16,18 @@ from app.data_sources.prices.kenya_market_price_adapter import (
     KenyaMarketPriceAdapter,
     KenyaMarketPriceRecord,
 )
+from app.data_sources.prices.wfp_kenya_price_adapter import (
+    WFP_KENYA_FOOD_PRICES_CSV_URL,
+    WfpKenyaPriceAdapter,
+)
 
 __all__ = [
     "HttpCsvKenyaMarketPriceAdapter",
     "KenyaMarketPriceAdapter",
     "KenyaMarketPriceRecord",
     "RawPriceRecord",
+    "WFP_KENYA_FOOD_PRICES_CSV_URL",
+    "WfpKenyaPriceAdapter",
     "normalize_price_text",
     "normalize_raw_price_record",
     "parse_optional_float",
